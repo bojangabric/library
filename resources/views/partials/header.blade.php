382dd407ef6674e622484d59a838f74753e9b724
bojangabric <bojan@bojangabric.com>
2019-10-21 18:15:42 +0200

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,51 +1,45 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-        <img src="/favicon.png" class="header-icon">
-        <a class="navbar-brand ml-5 header-library" href="/">Library</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li>
-                    <form class="my-2 my-lg-0 mr-lg-2" method="GET" action="/store/categories/1">
-                        <div class="input-group">
-                            <input class="form-control" type="text" name="keyword" placeholder="Search for a book...">
-                            <span class="input-group-append">
-                                <button class="btn btn-primary" type="submit">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                        </div>
+<nav class="bg-gray-900 p-6">
+    <div class="flex items-center justify-between flex-wrap w-4/6 mx-auto">
+        <div class="flex items-center  text-white mr-6">
+            <a href="/" class="flex">
+                <img src="/favicon.png" class="header-icon">
+                <span class="font-semibold text-xl tracking-tight">Library</span>
+            </a>
+        </div>
+        <div class="block lg:hidden">
+            <button class="flex items-center px-3 py-2 border rounded text-white border-teal-400 hover:text-gray-400 hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+            </button>
+        </div>
+        <div class="w-full block lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex-grow">
+                <div class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+                    <form method="GET" action="/store/categories/1">
+                        <input class="p-2 mr-0 rounded-l outline-none" type="text" name="keyword" placeholder="Search for a book...">
+                        <button class="text-white bg-blue-500 px-3 py-2 -ml-1 rounded-r outline-none" type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>
                     </form>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/store/categories/1?sort=&amp;gridview=">Shop</a>
-                </li>
+                </div>
+                <a href="/store/categories/1" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 text-base font-semibold">
+                    Shop
+                </a>
                 @auth
                 @if (!Auth::user()->isAdmin())
-                <li class="nav-item">
-                    <a class="nav-link" href="/cart">Cart</a>
-                </li>
+                <a class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 text-base font-semibold" href="/cart">Cart</a>
                 @endif
                 @endauth
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
+                <a class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 text-base font-semibold" href="/contact">Contact</a>
 
                 @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
+                <a class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 text-base font-semibold" href="/login">Login</a>
+                <a class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-400 mr-4 text-base font-semibold" href="/register">Register</a>
                 @else
                 <li class="nav-item dropdown header">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                         <span class="caret"></span>
                     </a>
@@ -65,8 +59,7 @@
                     </div>
                 </li>
                 @endguest
-
-            </ul>
+            </div>
         </div>
     </div>
 </nav>
@@ -74,7 +67,7 @@
 <script>
     var checkvalue = window.location.pathname;
 
-    $(".nav-link").each(function () {
+    $(".nav-link").each(function() {
         if ($(this).attr('href') == checkvalue) {
             $(this).addClass("active");
         }
