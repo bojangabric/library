beb6b066971b814864b5278c7f07e56e9e0c39bd
bojangabric <bojan@bojangabric.com>
2019-10-21 18:15:41 +0200

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,11 @@
-<footer class="bg-dark">
-    <div class="container text-center text-white" style="position:relative;">
+<footer class="bg-gray-900">
+    <div class="text-center text-white w-4/6 text-center mx-auto" style="position:relative;">
         Copyright &copy; Library {{ date('Y') }}
         <div style="position:absolute;right:0;top:0;margin-left:110px;text-align:right;">
-            Follow us on: 
+            Follow us on:
             <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
             <a href="https://twitter.com/" class="fa fa-twitter"></a>
             <a href="https://www.instagram.com/?hl=en" class="fa fa-instagram"></a>
         </div>
     </div>
 </footer>
-
