1b5c968dd8583c51c70aa8eeb66567315776a1ef
bojangabric <bojan@bojangabric.com>
2019-10-21 18:10:37 +0200

    Fix typo

--- database/factories/Books.php
+++ database/factories/Books.php
@@ -8,10 +8,10 @@ $factory->define(App\Book::class, function (Faker $faker) {
         'Name' => $faker->name,
         'Price' => $faker->randomDigit,
         'Quantity' => $faker->randomDigit,
-        'CategoryID' => $faker->numberBetween($min=1,$max=11),
-        'StarRating' => $faker->numberBetween($min=1,$max=5),
+        'CategoryID' => $faker->numberBetween($min = 1, $max = 11),
+        'StarRating' => $faker->numberBetween($min = 1, $max = 5),
         'DateOfPublication' => $faker->date($format = 'Y-m-d', $max = 'now'),
-        'Image' => $faker->randomElement($array = array ('the_diary_of_a_young_girl.jpg','steve_jobs.jpg','it.jpg', 'murder_on_the_orient_express.jpg', 'phpthe_hitchhikers_guide_to_the_galaxy.jpg')),
+        'Image' => $faker->randomElement($array = array('the_diary_of_a_young_girl.jpg', 'steve_jobs.jpg', 'it.jpg', 'murder_on_the_orient_express.jpg', 'the_hitchhikers_guide_to_the_galaxy.jpg')),
         'BookDescription' => $faker->text($maxNbChars = 200)
     ];
 });
