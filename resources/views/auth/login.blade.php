38a869ba35c581121fdae7e76d6623f326756bd9
bojangabric <bojan@bojangabric.com>
2019-10-27 14:35:19 +0100

    Redesign login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,70 +3,52 @@
 @section('title', 'Login')
 @section('content')
 
-<br>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<form class="mx-auto mt-8 p-8 bg-white shadow-md rounded w-full max-w-md" method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+  @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+        {{ __('E-Mail Address') }}
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" name="email" id="email" type="email" placeholder="jane@example.com" autofocus required>
+      @if ($errors->has('email'))
+      <span class="invalid-feedback text-red-500 text-xs italic" role="alert">
+        {{ $errors->first('email') }}
+      </span>
+      @endif
     </div>
-</div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-3">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
+        {{ __('Password') }}
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-400" name="password" id="password" type="password" placeholder="******************" required>
+      @if ($errors->has('password'))
+      <span class="invalid-feedback text-red-500 text-xs italic" role="alert">
+        {{ $errors->first('password') }}
+      </span>
+      @endif
+    </div>
+  </div>
+
+  <div class="mb-6">
+    <label class="text-gray-500 font-semibold flex items-center">
+      <input class="mr-1  leading-tight" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+    </label>
+  </div>
+
+  <div class="flex items-center justify-between">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+      {{ __('Login') }}
+    </button>
+    <a class="inline-block align-baseline font-semibold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
+      {{ __('Forgot Your Password?') }}
+    </a>
+  </div>
+
+</form>
+
 @endsection
