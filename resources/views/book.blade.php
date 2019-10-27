6d2afdb28fa34a6fbcebf147c6a3a4cfab02ce9b
bojangabric <bojan@bojangabric.com>
2019-10-27 20:09:49 +0100

    Redesign single book view

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -2,41 +2,41 @@
 
 @section('title', $book->Name)
 @section('content')
-
-<div class="container">
-  <div class="row">
-    @include('partials.categories')
-    <div class="col-lg-9">
-      <div class="mt-4">
-        <img class="card-img-top bookpic" src="/images/{{ $book->Image }}" alt="">
-        <div class="card-body">
-          <h3 class="card-title book" id="bookName"><strong>{{ $book->Name }}</strong></h3>
-            <?php 
-            $author = App\Author::where("AuthorID", "=", $book->AuthorID)->first(); 
-            ?>
-          <bold class="author-name">{{ $author->Name }} {{ $author->Surname }}</bold><br>
-          <span class="card-rating">{{ html_entity_decode($book->getRating()) }}</span>
-        </div>
-
-        <h4>${{ $book->Price }}</h4>
-        <div class="btn-group mt-4">
-        <?php
-        if (Auth::user() && DB::table('shopping_carts')->where('user_id', Auth::user()->id)->where('BookID', $book->BookID)->exists()) {
-          echo '<button type="button" class="btn btn-primary disabled">In cart</button>';
-        } else {
-          if (DB::table('books')->where('BookID', $book->BookID)->where('Quantity', '>', 0)->exists()) {
-            echo '<button type="button" class="btn btn-primary">Buy</button>
+<div class="mt-4 w-2/5 mx-auto">
+  <div class="flex">
+    <img class="w-48 rounded" src="/images/{{ $book->Image }}" alt="">
+    <div class="ml-3">
+      <div class="text-4xl font-bold" id="bookName">{{ $book->Name }}</div>
+      <?php
+      $author = App\Author::where("AuthorID", "=", $book->AuthorID)->first();
+      ?>
+      <bold class="author-name">{{ $author->Name }} {{ $author->Surname }}</bold><br>
+      <span class="text-yellow-500 text-3xl">{{ html_entity_decode($book->getRating()) }}</span>
+      <div class="font-semibold text-2xl text-gray-800">${{ $book->Price }}</div>
+      <div class="inline-flex text-white relative">
+        <!-- <?php
+              if (Auth::user() && DB::table('shopping_carts')->where('user_id', Auth::user()->id)->where('BookID', $book->BookID)->exists()) {
+                echo '<button type="button" class="btn btn-primary disabled">In cart</button>';
+              } else {
+                if (DB::table('books')->where('BookID', $book->BookID)->where('Quantity', '>', 0)->exists()) {
+                  echo '<button type="button" class="btn btn-primary">Buy</button>
               <button class="btn btn-primary btn-cart">Add to cart</button>';
-          } else {
-            echo '<button type="button" class="btn btn-primary disabled">Sold out</button>';
-          }
-        }
-        ?>
-        </div>
-        <p class="card-text"><br>{{ $book->BookDescription }}</p>
-        @include('partials.reviews')
+                } else {
+                  echo '<button type="button" class="btn btn-primary disabled">Sold out</button>';
+                }
+              }
+              ?> -->
+
+        <button class="bg-blue-500 border-r border-blue-400 hover:bg-blue-600 font-semibold py-2 px-4 rounded-l">
+          Buy
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-600 font-semibold py-2 px-4 rounded-r btn-cart">
+          Add to cart
+        </button>
       </div>
-    </div> 
+    </div>
   </div>
+  <div class="mt-6">{{ $book->BookDescription }}</div>
+  @include('partials.reviews')
 </div>
 @endsection
