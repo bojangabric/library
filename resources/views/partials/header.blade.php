172a102b5ca301a10efa44f40a8c660babf8ee58
bojangabric <bojan@bojangabric.com>
2019-10-29 15:45:20 +0100

    Make header responsive

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-gray-900 p-6">
-  <div class="flex items-center justify-between flex-wrap w-4/6 mx-auto">
+  <div class="flex items-center justify-between flex-wrap xl:w-5/6 mx-auto">
     <div class="flex items-center  text-white mr-6">
       <a href="/" class="flex">
         <img src="/favicon.png" class="header-icon">
@@ -7,16 +7,15 @@
       </a>
     </div>
     <div class="block lg:hidden">
-      <button class="flex items-center px-3 py-2 border rounded text-white border-teal-400 hover:text-gray-400 hover:border-white">
+      <button class="flex items-center px-3 py-2 border rounded text-white border-teal-400 hover:text-gray-400 hover:border-white" id="menu">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
       </button>
     </div>
-    <div class="w-full block lg:flex lg:items-center lg:w-auto">
+    <div class="w-full lg:flex lg:items-center lg:w-auto hidden links">
       <div class="text-sm lg:flex-grow">
-        <div class="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        <div class="block mt-4 lg:inline-block lg:mt-0 w-full lg:w-auto mr-4">
           <form method="GET" action="/store/categories/1">
             <input class="p-2 mr-0 rounded-l outline-none" type="text" name="keyword" placeholder="Search for a book...">
             <button class="text-white bg-blue-500 px-3 py-2 -ml-1 rounded-r outline-none" type="submit">
@@ -43,7 +42,7 @@
             {{ Auth::user()->name }}
             <i class="fa fa-caret-down"></i>
           </div>
-          <div class="mt-1 absolute bg-gray-900 rounded border border-gray-800 overflow-hidden flex flex-col username-options hidden">
+          <div class="mt-1 absolute bg-gray-900 rounded border border-gray-800 overflow-hidden flex flex-col username-options hidden z-10">
             @if (Auth::user()->isAdmin())
             <a class="p-3 hover:text-gray-400 border-b border-gray-800" href="/admin">Admin dashboard</a>
             @endif
@@ -73,6 +72,10 @@
 
   $(".username").click(function() {
     $('.username-options').removeClass("hidden");
+  });
+
+  $("#menu").on('click', function() {
+    $(".links").toggleClass("hidden");
   });
 
   window.onclick = function(event) {
