8fd3b1f2ff91d6e14754c91d6a458884c8979a89
bojangabric <bojan@bojangabric.com>
2019-10-29 15:46:01 +0100

    Remove absolute element from footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="bg-gray-900 absolute bottom-0 w-full py-3">
-    <div class="text-center text-white w-4/6 text-center mx-auto" style="position:relative;">
-        Copyright &copy; Library {{ date('Y') }}
-        <div style="position:absolute;right:0;top:0;margin-left:110px;text-align:right;">
+    <div class="text-center text-white w-5/6 text-center mx-auto">
+        <div>
             Follow us on:
             <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
             <a href="https://twitter.com/" class="fa fa-twitter"></a>
             <a href="https://www.instagram.com/?hl=en" class="fa fa-instagram"></a>
         </div>
+        Copyright &copy; Library {{ date('Y') }}
     </div>
 </footer>
