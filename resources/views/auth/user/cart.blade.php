658a4b6e53ba96e60d2b6c58e722ad8232b7bb91
bojangabric <bojan@bojangabric.com>
2019-10-29 15:44:21 +0100

    Redesign cart page

--- resources/views/auth/user/cart.blade.php
+++ resources/views/auth/user/cart.blade.php
@@ -1,64 +1,71 @@
-@extends('layouts.master') 
+@extends('layouts.master')
 
-@section('title', 'Shopping cart') 
-@section('content') 
+@section('title', 'Shopping cart')
+@section('content')
 <?php
-  $books = DB::table('shopping_carts')->where('user_id', Auth::user()->id)->pluck('BookID');
-  $bookNames = [];
-  $bookIDs = [];
-  $bookPrices = [];
-  foreach ($books as $bookid) {
-    $bookNames[] = DB::table('books')->where('BookID', $bookid)->pluck('Name');
-    $bookIDs[] = $bookid;
-    $bookPrices[] = DB::table('books')->where('BookID', $bookid)->pluck('Price');
-  }
+$books = DB::table('shopping_carts')->where('user_id', Auth::user()->id)->pluck('BookID');
+$bookNames = [];
+$bookIDs = [];
+$bookPrices = [];
+foreach ($books as $bookid) {
+  $bookNames[] = DB::table('books')->where('BookID', $bookid)->pluck('Name');
+  $bookIDs[] = $bookid;
+  $bookPrices[] = DB::table('books')->where('BookID', $bookid)->pluck('Price');
+}
 
 ?>
 
-<div class="container ml-5 mt-4">
-    <div class="col-lg-9 offset-5">
-      <div class="shopping-cart">Shopping cart</div>
-      <table class="table table-hover cart-table">
-        <thead>
-          <tr>
-            <th>Book</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-            if (empty($bookIDs))
-              echo '<tr><td>There are no books in your shopping cart.</td><td></td></tr>';
-            for ($i=0; $i < count($bookIDs); $i++) { 
-              echo '<tr>';
-              echo '<td><a href="/store/book/' . $bookIDs[$i] .'">' . $bookNames[$i][0] . '</a></td>';
-              echo '<td style="font-size: 12pt;">$' . $bookPrices[$i][0] . '</td>';
-              echo '<td align="center"><i class="delete-book fa fa-minus-circle"></i></td>';
-              echo '</tr>';
-            }
-          ?>
-          <tr>
-            <td>
-              <button class="btn btn-primary">Checkout</button>
-            </td>
-            <td>
-            <?php
-                $total = 0;
-                foreach ($bookPrices as $bookPrice){
-                  $total += $bookPrice[0];
-                }
-                echo 'Total: $' . $total;
-              ?>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+<div class="mx-auto mt-8 p-8 pb-5 bg-white shadow-md rounded w-full max-w-xl">
+  <table class="w-full">
+    <thead>
+      <tr>
+        <th class="px-4 py-2 text-left">Book</th>
+        <th class="px-4 py-2 text-right">Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      @if (empty($bookIDs))
+      <tr>
+        <td class="border px-4 py-2">There are no books in your shopping cart.</td>
+        <td class="border px-4 py-2"></td>
+      </tr>
+      @endif
+
+      @for ($i = 0; $i < count($bookIDs); $i++) <tr>
+        <td class="border px-4 py-2 text-left">
+          <a href="/store/book/{{ $bookIDs[$i] }}" class="text-blue-500 hover:text-blue-600">{{ $bookNames[$i][0] }}</a>
+        </td>
+        <td class="border px-4 py-2 text-right">
+          ${{ $bookPrices[$i][0] }}
+        </td>
+        <td class="text-right pl-2 sm:pl-0">
+          <i class="delete-book fa fa-minus-circle cursor-pointer"></i>
+        </td>
+        </tr>
+        @endfor
+    </tbody>
+  </table>
+
+  <div class="flex items-center justify-between mt-8 px-3">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-8 rounded focus:outline-none focus:shadow-outline" type="submit">
+      {{ __('Checkout') }}
+    </button>
+    <div>
+      <?php
+      $total = 0;
+      foreach ($bookPrices as $bookPrice) {
+        $total += $bookPrice[0];
+      }
+      ?>
+      <div class="text-xl">Total: <span class="font-semibold text-2xl">${{$total}}</span></div>
     </div>
+  </div>
 </div>
+
 
 <script>
   $(function() {
-    $(".delete-book").on("click", function () {
+    $(".delete-book").on("click", function() {
       var bookname = $(this).closest("tr").find('td:first').text();
       $(this).closest("tr").remove();
 
