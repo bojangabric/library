c541157f6e33e899c68ae404bddc4225168a6a9f
bojangabric <bojan@bojangabric.com>
2020-01-08 16:59:41 +0100

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,59 @@
-<footer class="bg-gray-900 absolute bottom-0 w-full py-3">
-    <div class="text-center text-white w-5/6 text-center mx-auto">
-        <div>
-            Follow us on:
-            <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
-            <a href="https://twitter.com/" class="fa fa-twitter"></a>
-            <a href="https://www.instagram.com/?hl=en" class="fa fa-instagram"></a>
-        </div>
-        Copyright &copy; Library {{ date('Y') }}
+<div class="pt-16 lg:pt-32 pb-24 bg-gray-900 font-light text-white">
+  <div class="w-4/5 lg:w-3/5 mx-auto lg:flex justify-between">
+    <div class="mb-12 lg:w-1/3 lg:mr-48">
+      <a href="{{ url('/') }}">
+        Library
+      </a>
+      <div class="mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse onsectetur adipisicing elit.</div>
     </div>
-</footer>
+    <div class="w-full lg:w-2/3 flex flex-wrap justify-between">
+      <div class="mb-8 text-gray-300">
+        <div class="font-medium mb-4 text-white">About us</div>
+        <a href="#!" class="block mb-2 hover:text-white">Partner Help</a>
+        <a href="#!" class="block mb-2 hover:text-white">Careers</a>
+        <a href="#!" class="block mb-2 hover:text-white">Press Center</a>
+        <a href="#!" class="block mb-2 hover:text-white">Investor Relations</a>
+        <a href="#!" class="block mb-2 hover:text-white">Terms&Conditions</a>
+      </div>
+      <div class="mb-8 text-gray-300">
+        <div class="font-medium mb-4 text-white">Quicklinks</div>
+        <a href="#!" class="block mb-2 hover:text-white">Places of Interest</a>
+        <a href="#!" class="block mb-2 hover:text-white">Apartments</a>
+        <a href="#!" class="block mb-2 hover:text-white">Guest houses</a>
+      </div>
+      <div class="mb-8 text-gray-300">
+        <div class="font-medium mb-4 text-white">Contact</div>
+        <div class="mb-4 hover:text-white">example@mail.com</div>
+        <div class="">789-231-1141</div>
+        <div class="">161 Limer Street</div>
+        <div class="">59201 Montana, USA</div>
+      </div>
+    </div>
+  </div>
+  <div class="mt-6 lg:mt-20 w-4/5 lg:w-3/5 mx-auto flex flex-wrap justify-between items-center">
+    <div class="order-2 lg:order-1 mt-12 lg:mt-0">Copyright © 2019-{{ date('Y') }} Library.com™. All rights reserved.</div>
+    <div class="flex justify-between order-1 lg:order-2">
+      <div class="h-10 w-10 bg-gray-100 rounded-full items-center flex justify-center mr-3">
+        <svg class="w-5 h-5 fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96.124 96.123">
+          <path d="M72.089.02L59.624 0C45.62 0 36.57 9.285 36.57 23.656v10.907H24.037a1.96 1.96 0 00-1.96 1.961v15.803a1.96 1.96 0 001.96 1.96H36.57v39.876a1.96 1.96 0 001.96 1.96h16.352a1.96 1.96 0 001.96-1.96V54.287h14.654a1.96 1.96 0 001.96-1.96l.006-15.803a1.963 1.963 0 00-1.961-1.961H56.842v-9.246c0-4.444 1.059-6.7 6.848-6.7l8.397-.003a1.96 1.96 0 001.959-1.96V1.98A1.96 1.96 0 0072.089.02z" />
+        </svg>
+      </div>
+      <div class="h-10 w-10 bg-gray-100 rounded-full items-center flex justify-center mr-3">
+        <svg class="w-5 h-5 fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path d="M512 97.248c-19.04 8.352-39.328 13.888-60.48 16.576 21.76-12.992 38.368-33.408 46.176-58.016-20.288 12.096-42.688 20.64-66.56 25.408C411.872 60.704 384.416 48 354.464 48c-58.112 0-104.896 47.168-104.896 104.992 0 8.32.704 16.32 2.432 23.936-87.264-4.256-164.48-46.08-216.352-109.792-9.056 15.712-14.368 33.696-14.368 53.056 0 36.352 18.72 68.576 46.624 87.232-16.864-.32-33.408-5.216-47.424-12.928v1.152c0 51.008 36.384 93.376 84.096 103.136-8.544 2.336-17.856 3.456-27.52 3.456-6.72 0-13.504-.384-19.872-1.792 13.6 41.568 52.192 72.128 98.08 73.12-35.712 27.936-81.056 44.768-130.144 44.768-8.608 0-16.864-.384-25.12-1.44C46.496 446.88 101.6 464 161.024 464c193.152 0 298.752-160 298.752-298.688 0-4.64-.16-9.12-.384-13.568 20.832-14.784 38.336-33.248 52.608-54.496z" />
+        </svg>
+      </div>
+      <div class="h-10 w-10 bg-gray-100 rounded-full items-center flex justify-center mr-3">
+        <svg class="w-5 h-5 fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 169.063 169.063">
+          <path d="M122.406 0H46.654C20.929 0 0 20.93 0 46.655v75.752c0 25.726 20.929 46.655 46.654 46.655h75.752c25.727 0 46.656-20.93 46.656-46.655V46.655C169.063 20.93 148.133 0 122.406 0zm31.657 122.407c0 17.455-14.201 31.655-31.656 31.655H46.654C29.2 154.063 15 139.862 15 122.407V46.655C15 29.201 29.2 15 46.654 15h75.752c17.455 0 31.656 14.201 31.656 31.655v75.752z" />
+          <path d="M84.531 40.97c-24.021 0-43.563 19.542-43.563 43.563 0 24.02 19.542 43.561 43.563 43.561s43.563-19.541 43.563-43.561c0-24.021-19.542-43.563-43.563-43.563zm0 72.123c-15.749 0-28.563-12.812-28.563-28.561 0-15.75 12.813-28.563 28.563-28.563s28.563 12.813 28.563 28.563c0 15.749-12.814 28.561-28.563 28.561zM129.921 28.251c-2.89 0-5.729 1.17-7.77 3.22a11.053 11.053 0 00-3.23 7.78c0 2.891 1.18 5.73 3.23 7.78 2.04 2.04 4.88 3.22 7.77 3.22 2.9 0 5.73-1.18 7.78-3.22 2.05-2.05 3.22-4.89 3.22-7.78 0-2.9-1.17-5.74-3.22-7.78-2.04-2.05-4.88-3.22-7.78-3.22z" />
+        </svg>
+      </div>
+      <div class="h-10 w-10 bg-gray-100 rounded-full items-center flex justify-center">
+        <svg class="w-5 h-5 fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 310">
+          <path d="M72.16 99.73H9.927a5 5 0 00-5 5v199.928a5 5 0 005 5H72.16a5 5 0 005-5V104.73a5 5 0 00-5-5zM41.066.341C18.422.341 0 18.743 0 41.362 0 63.991 18.422 82.4 41.066 82.4c22.626 0 41.033-18.41 41.033-41.038C82.1 18.743 63.692.341 41.066.341zM230.454 94.761c-24.995 0-43.472 10.745-54.679 22.954V104.73a5 5 0 00-5-5h-59.599a5 5 0 00-5 5v199.928a5 5 0 005 5h62.097a5 5 0 005-5V205.74c0-33.333 9.054-46.319 32.29-46.319 25.306 0 27.317 20.818 27.317 48.034v97.204a5 5 0 005 5H305a5 5 0 005-5V194.995c0-49.565-9.451-100.234-79.546-100.234z" />
+        </svg>
+      </div>
+    </div>
+  </div>
+</div>
