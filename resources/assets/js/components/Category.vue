89c59e2b2b00867bed8446c5207b8a91fe35f875
bojangabric <bojan@bojangabric.com>
2020-01-08 16:58:48 +0100

    Add Category vue component

---
+++ resources/assets/js/components/Category.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full sm:w-1/3 pr-2">
+    <div class="rounded shadow-md overflow-hidden">
+      <img
+        style="height:18rem;"
+        class="w-full object-fill object-center"
+        :src="'images/' + image + '.jpg'"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["image"]
+};
+</script>
