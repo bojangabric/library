a66dad3fd79da38cf6016840c6875aebdd092928
bojangabric <bojan@bojangabric.com>
2020-01-08 17:00:15 +0100

    Add vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,26 @@
-require("./jquery");
+import Flickity from "vue-flickity";
+import "./bootstrap";
+window.Vue = require("vue");
+
+Vue.component("book", require("./components/Book.vue"));
+Vue.component("category", require("./components/Category.vue"));
+Vue.component(
+  "popular-categories",
+  require("./components/PopularCategories.vue")
+);
+
+const app = new Vue({
+  el: "#app",
+  components: {
+    Flickity
+  },
+  data() {
+    return {
+      flickityOptions: {
+        prevNextButtons: true,
+        pageDots: false,
+        wrapAround: true
+      }
+    };
+  }
+});
