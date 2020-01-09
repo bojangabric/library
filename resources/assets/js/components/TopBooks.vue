c2cb24c3e9c04519a85eba35809375c70888093e
bojangabric <bojan@bojangabric.com>
2020-01-09 20:16:53 +0100

    Add TopBooks vue component

---
+++ resources/assets/js/components/TopBooks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-2/3">
+    <div class="text-4xl font-black uppercase text-gray-800 mb-6">
+      Top books
+    </div>
+    <div class="flex flex-wrap">
+      <top-book></top-book>
+      <top-book></top-book>
+      <top-book></top-book>
+      <top-book></top-book>
+      <top-book></top-book>
+      <top-book></top-book>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
