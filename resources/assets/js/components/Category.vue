0dd892b70a0667d31945bb65e83a73d10c0b4cf8
bojangabric <bojan@bojangabric.com>
2020-01-09 20:14:13 +0100

    Update Category height

--- resources/assets/js/components/Category.vue
+++ resources/assets/js/components/Category.vue
@@ -2,7 +2,7 @@
   <div class="w-full sm:w-1/3 pr-2">
     <div class="rounded shadow-md overflow-hidden">
       <img
-        style="height:18rem;"
+        style="height:16rem;"
         class="w-full object-fill object-center"
         :src="'images/' + image + '.jpg'"
         alt=""
