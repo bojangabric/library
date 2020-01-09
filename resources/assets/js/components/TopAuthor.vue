1d0ca9c44f6a148faa6c8168d79d1764b5d7d86d
bojangabric <bojan@bojangabric.com>
2020-01-09 20:16:19 +0100

    Add TopAuthor vue component

---
+++ resources/assets/js/components/TopAuthor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mb-12 flex">
+    <img
+      class="w-24 h-24 object-cover rounded"
+      src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=144&h=144&q=80"
+    />
+    <div class="ml-2">
+      <div class="text-gray-800 font-medium leading-none">
+        Edgar Allan Poe
+      </div>
+      <div class="text-gray-500 mb-1">
+        American short story author
+      </div>
+      <div
+        class="sm:text-sm border bg-gray-200 rounded px-2 py-1 inline-block mt-2"
+      >
+        Read more
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
