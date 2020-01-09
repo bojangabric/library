43cf76cda4dc37889512e5476cbaabc083053a35
bojangabric <bojan@bojangabric.com>
2020-01-09 20:17:05 +0100

    Add AudioBooks vue component

---
+++ resources/assets/js/components/AudioBooks.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="w-full">
+    <div class="text-4xl font-black uppercase text-gray-800 mb-6">
+      Audio books
+    </div>
+    <div class="flex items-center mb-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="h-6w w-6 fill-current text-gray-200"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path
+          class="text-gray-800"
+          d="M15.51 11.14a1 1 0 0 1 0 1.72l-5 3A1 1 0 0 1 9 15V9a1 1 0 0 1 1.51-.86l5 3z"
+        />
+      </svg>
+      <div class="ml-2">
+        <div class="uppercase tracking-wide font-semibold leading-none w-40">
+          The best of me
+        </div>
+        <div class="text-gray-500 font-light">
+          By Someone
+        </div>
+      </div>
+      <div class="ml-12 font-semibold text-xl">
+        $25
+      </div>
+      <div class="leading-none text-xs ml-2 mr-6">
+        Full<br />
+        version
+      </div>
+      <div class="border bg-gray-200 rounded px-4 py-2">
+        Buy now
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="h-6w w-6 fill-current text-gray-200"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path
+          class="text-gray-800"
+          d="M15.51 11.14a1 1 0 0 1 0 1.72l-5 3A1 1 0 0 1 9 15V9a1 1 0 0 1 1.51-.86l5 3z"
+        />
+      </svg>
+      <div class="ml-2">
+        <div class="uppercase tracking-wide font-semibold leading-none w-40">
+          The best of me
+        </div>
+        <div class="text-gray-500 font-light">
+          By Someone
+        </div>
+      </div>
+      <div class="ml-12 font-semibold text-xl">
+        $25
+      </div>
+      <div class="leading-none text-xs ml-2 mr-6">
+        Full<br />
+        version
+      </div>
+      <div class="border bg-gray-200 rounded px-4 py-2">
+        Buy now
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="h-6w w-6 fill-current text-gray-200"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path
+          class="text-gray-800"
+          d="M15.51 11.14a1 1 0 0 1 0 1.72l-5 3A1 1 0 0 1 9 15V9a1 1 0 0 1 1.51-.86l5 3z"
+        />
+      </svg>
+      <div class="ml-2">
+        <div class="uppercase tracking-wide font-semibold leading-none w-40">
+          The best of me
+        </div>
+        <div class="text-gray-500 font-light">
+          By Someone
+        </div>
+      </div>
+      <div class="ml-12 font-semibold text-xl">
+        $25
+      </div>
+      <div class="leading-none text-xs ml-2 mr-6">
+        Full<br />
+        version
+      </div>
+      <div class="border bg-gray-200 rounded px-4 py-2">
+        Buy now
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="h-6w w-6 fill-current text-gray-200"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path
+          class="text-gray-800"
+          d="M15.51 11.14a1 1 0 0 1 0 1.72l-5 3A1 1 0 0 1 9 15V9a1 1 0 0 1 1.51-.86l5 3z"
+        />
+      </svg>
+      <div class="ml-2">
+        <div class="uppercase tracking-wide font-semibold leading-none w-40">
+          The best of me
+        </div>
+        <div class="text-gray-500 font-light">
+          By Someone
+        </div>
+      </div>
+      <div class="ml-12 font-semibold text-xl">
+        $25
+      </div>
+      <div class="leading-none text-xs ml-2 mr-6">
+        Full<br />
+        version
+      </div>
+      <div class="border bg-gray-200 rounded px-4 py-2">
+        Buy now
+      </div>
+    </div>
+    <div class="flex items-center mb-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="h-6w w-6 fill-current text-gray-200"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <path
+          class="text-gray-800"
+          d="M15.51 11.14a1 1 0 0 1 0 1.72l-5 3A1 1 0 0 1 9 15V9a1 1 0 0 1 1.51-.86l5 3z"
+        />
+      </svg>
+      <div class="ml-2">
+        <div class="uppercase tracking-wide font-semibold leading-none w-40">
+          The best of me
+        </div>
+        <div class="text-gray-500 font-light">
+          By Someone
+        </div>
+      </div>
+      <div class="ml-12 font-semibold text-xl">
+        $25
+      </div>
+      <div class="leading-none text-xs ml-2 mr-6">
+        Full<br />
+        version
+      </div>
+      <div class="border bg-gray-200 rounded px-4 py-2">
+        Buy now
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
