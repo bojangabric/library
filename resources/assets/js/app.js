f7b43ac6fa66514bed996ee26812ab68d6feeb1f
bojangabric <bojan@bojangabric.com>
2020-01-09 20:17:16 +0100

    Register vue components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,15 @@ window.Vue = require("vue");
 
 Vue.component("book", require("./components/Book.vue"));
 Vue.component("category", require("./components/Category.vue"));
+Vue.component("audio-books", require("./components/AudioBooks.vue"));
+Vue.component("top-books", require("./components/TopBooks.vue"));
+Vue.component("top-book", require("./components/TopBook.vue"));
+Vue.component("top-authors", require("./components/TopAuthors.vue"));
+Vue.component("top-author", require("./components/TopAuthor.vue"));
+Vue.component(
+  "books-for-children",
+  require("./components/BooksForChildren.vue")
+);
 Vue.component(
   "popular-categories",
   require("./components/PopularCategories.vue")
