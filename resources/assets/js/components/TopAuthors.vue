b83b2c6361357f574098b5070f49d6cada4d6d45
bojangabric <bojan@bojangabric.com>
2020-01-09 20:16:46 +0100

    Add TopAuthros vue component

---
+++ resources/assets/js/components/TopAuthors.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-1/3">
+    <div class="text-4xl font-black uppercase text-gray-800 mb-6">
+      Top authors
+    </div>
+    <top-author></top-author>
+    <top-author></top-author>
+    <top-author></top-author>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
