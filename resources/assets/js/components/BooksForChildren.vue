5dcd07b312eb50abae6790293ffbfed420340480
bojangabric <bojan@bojangabric.com>
2020-01-10 21:10:04 +0100

    Update BooksForChildren cover images

--- resources/assets/js/components/BooksForChildren.vue
+++ resources/assets/js/components/BooksForChildren.vue
@@ -8,25 +8,25 @@
       style="grid-template-columns: 300px repeat(3, 1fr);grid-template-rows: repeat(2, 200px);grid-column-gap: 20px;grid-row-gap: 20px;"
     >
       <div style="grid-area: 1 / 1 / 3 / 2;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/harry-potter.jpg" />
       </div>
       <div style="grid-area: 1 / 2 / 2 / 3;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/power-of-love.jpg" />
       </div>
       <div style="grid-area: 1 / 3 / 2 / 4;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/redwall.jpg" />
       </div>
       <div style="grid-area: 1 / 4 / 2 / 5;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/the-mysterious-lake-bandit.jpg" />
       </div>
       <div style="grid-area: 2 / 2 / 3 / 3;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/little-red-riding-hood.jpg" />
       </div>
       <div style="grid-area: 2 / 3 / 3 / 4;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/three-little-pigs.jpg" />
       </div>
       <div style="grid-area: 2 / 4 / 3 / 5;">
-        <img class="w-full h-full object-cover rounded" src="images/it.jpg" />
+        <img class="w-full h-full object-fill rounded" src="images/books/the-last-kids-on-earth.jpg" />
       </div>
     </div>
   </div>
