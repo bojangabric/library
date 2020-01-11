7d43931fc2caec1689a99f69909526558b439774
bojangabric <bojan@bojangabric.com>
2020-01-11 22:34:26 +0100

    Add responsiveness to AudioBooks vue component

--- resources/assets/js/components/AudioBooks.vue
+++ resources/assets/js/components/AudioBooks.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full">
-    <div class="text-4xl font-black uppercase text-gray-800 mb-6">
+  <div class="w-full md:w-2/3 lg:w-1/2 sm:pb-0 md:pr-10">
+    <div class="text-2xl lg:text-3xl font-black uppercase text-gray-800 mb-6">
       Audio books
     </div>
     <audio-book title="The Nickel Boys" author="Colson Whitehead" price="25"></audio-book>
