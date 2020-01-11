238c9a0a58ff677296f156ba7f81968458e1bbc1
bojangabric <bojan@bojangabric.com>
2020-01-11 22:34:33 +0100

    Add responsiveness to BooksForChildren vue component

--- resources/assets/js/components/BooksForChildren.vue
+++ resources/assets/js/components/BooksForChildren.vue
@@ -1,31 +1,28 @@
 <template>
-  <div class="w-full mt-20">
-    <div class="text-4xl font-black uppercase text-gray-800 mb-6">
+  <div class="w-full">
+    <div class="text-2xl lg:text-3xl font-black uppercase text-gray-800 mb-6">
       Books for children
     </div>
-    <div
-      class="grid"
-      style="grid-template-columns: 300px repeat(3, 1fr);grid-template-rows: repeat(2, 200px);grid-column-gap: 20px;grid-row-gap: 20px;"
-    >
-      <div style="grid-area: 1 / 1 / 3 / 2;">
+    <div class="grid">
+      <div class="grid-1">
         <img class="w-full h-full object-fill rounded" src="images/books/harry-potter.jpg" />
       </div>
-      <div style="grid-area: 1 / 2 / 2 / 3;">
+      <div class="grid-2">
         <img class="w-full h-full object-fill rounded" src="images/books/power-of-love.jpg" />
       </div>
-      <div style="grid-area: 1 / 3 / 2 / 4;">
+      <div class="grid-3">
         <img class="w-full h-full object-fill rounded" src="images/books/redwall.jpg" />
       </div>
-      <div style="grid-area: 1 / 4 / 2 / 5;">
+      <div class="grid-4">
         <img class="w-full h-full object-fill rounded" src="images/books/the-mysterious-lake-bandit.jpg" />
       </div>
-      <div style="grid-area: 2 / 2 / 3 / 3;">
+      <div class="grid-5">
         <img class="w-full h-full object-fill rounded" src="images/books/little-red-riding-hood.jpg" />
       </div>
-      <div style="grid-area: 2 / 3 / 3 / 4;">
+      <div class="grid-6">
         <img class="w-full h-full object-fill rounded" src="images/books/three-little-pigs.jpg" />
       </div>
-      <div style="grid-area: 2 / 4 / 3 / 5;">
+      <div class="grid-7">
         <img class="w-full h-full object-fill rounded" src="images/books/the-last-kids-on-earth.jpg" />
       </div>
     </div>
