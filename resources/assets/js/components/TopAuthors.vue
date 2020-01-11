54a641e1488234b4613a47341af425b1f522b38d
bojangabric <bojan@bojangabric.com>
2020-01-11 22:31:07 +0100

    Add responsiveness to TopAuthors vue component

--- resources/assets/js/components/TopAuthors.vue
+++ resources/assets/js/components/TopAuthors.vue
@@ -1,31 +1,25 @@
 <template>
-  <div class="w-1/3">
+  <div class="w-full xl:w-1/3 sm:pb-0 pt-16 xl:pt-0">
     <div class="flex items-center pb-6">
-      <div class="text-4xl font-black uppercase text-gray-800">
+      <div class="text-2xl lg:text-3xl font-black uppercase text-gray-800 whitespace-no-wrap">
         Top authors
       </div>
-      <a
-        href="#!"
-        class="ml-4 sm:text-sm border bg-gray-200 text-gray-800 rounded px-2 py-1 mt-1 hover:bg-gray-300 transition-colors ease-in duration-100"
-      >
-        Show all
-      </a>
     </div>
-    <top-author
-      name="J.K. Rowling"
-      description="British author, film producer, screenwriter, and philanthropist"
-      image="jk-rowling"
-    ></top-author>
-    <top-author
-      name="William Shakespeare"
-      description="Greatest writer in the English language"
-      image="shakespeare"
-    ></top-author>
-    <top-author
-      name="Stephen King"
-      description="American author of horror and supernatural fiction novels"
-      image="stephen-king"
-    ></top-author>
+    <div class="md:flex xl:block">
+      <top-author
+        class="mr-12"
+        name="J.K. Rowling"
+        description="British author and film producer"
+        image="jk-rowling"
+      ></top-author>
+      <top-author
+        class="mr-12"
+        name="William Shakespeare"
+        description="Greatest writer in the English language"
+        image="shakespeare"
+      ></top-author>
+      <top-author name="Stephen King" description="American author of horror novels" image="stephen-king"></top-author>
+    </div>
   </div>
 </template>
 
