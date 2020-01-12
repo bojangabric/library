d9566faddf29927cd5500fdc1fffab7bdd5c9d23
bojangabric <bojan@bojangabric.com>
2020-01-12 19:25:45 +0100

    Add Category vue component

---
+++ resources/assets/js/components/filter/Category.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center justify-between">
+    <div class="flex items-center">
+      <input type="checkbox" class="form-checkbox mr-1" :name="typeName.toLowerCase()" :id="typeName.toLowerCase()" />
+      <label class="ml-1 text-gray-700" :for="typeName.toLowerCase()">{{ typeName }}</label>
+    </div>
+    <div class="text-sm text-gray-700">{{ count }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['typeName', 'count']
+};
+</script>
